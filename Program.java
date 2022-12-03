9be1ab9978a83c28e0e95d68fb8df5485737a37c
Brahim Kellou <brahimkellou@outlook.com>
2022-12-03 20:16:10 +0100

    feat: add todo

--- Program.java
+++ Program.java
@@ -1,13 +1,13 @@
-import java.io.IOException;
 import java.util.Scanner;
 
 public class Program {
     public static void main(String[] args) {
         Session session = new Session();
+        Scanner sc = new Scanner(System.in);
 
         int option = 0;
         while (option != 6) {
-            Scanner sc = new Scanner(System.in);
+
             System.out.println("1 - Modifier le prix de location d'un vélo.");
             System.out.println("2 - Supprimer une location.");
             System.out.println("3 - Supprimer un vélo.");
@@ -25,8 +25,12 @@ public class Program {
                     session.deleteBooking();
                     break;
                 case 3:
+                    // TODO: implement session.deleteBicyle()
+                    // same as session.deleteBooking()
                     break;
                 case 4:
+                    // TODO: implement session.deleteClient()
+                    // same as session.deleteBooking()
                     break;
                 case 5:
                     session.bookBicyle();
@@ -36,9 +40,9 @@ public class Program {
                 default:
                     break;
             }
-            sc.close();
+
             // System.out.print("\033\143");
         }
-
+        sc.close();
     }
 }
